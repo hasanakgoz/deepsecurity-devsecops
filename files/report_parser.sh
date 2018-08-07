fee5feda74b75841233304dca3a57188283a0b0e
Markus Winkler <winkler.info@icloud.com>
2018-08-07 12:12:04 +0200

    improved parser to parse nessus reports

--- files/report_parser.sh
+++ files/report_parser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # call with ./report_parser.sh <report file>
-cat $1 | sed 's/.*\(CVE-[0-9]\+-[0-9]\+\).*/\1/' | grep "CVE-" | sort | uniq | awk '{printf("\"%s\" ", $0)}'
+#cat $1 | sed -n 's/.*\(CVE-[0-9]+-[0-9]\+\).*/\1/' | grep "CVE-" | sort | uniq | awk '{printf("\"%s\" ", $0)}'
+cat $1 | egrep -o 'CVE-[0-9]+-[0-9]+' | sort | uniq | awk '{printf("\"%s\" ", $0)}'
 echo
