a8ab24c1705f997154442014c803fad52bb18892
Markus Winkler <winkler.info@icloud.com>
2018-08-27 13:59:33 +0200

    obsolete

--- smartcheck/connect.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export POD_NAME=$(kubectl get pods --namespace smartcheck -l "service=proxy,release=deepsecurity-smartcheck" -o jsonpath="{.items[0].metadata.name}")
-echo Password: $(kubectl get --namespace smartcheck secrets -o jsonpath='{.data.password }' deepsecurity-smartcheck-auth | base64 --decode)
-kubectl proxy &
-kubectl port-forward --namespace smartcheck $POD_NAME 8443:8443 &
